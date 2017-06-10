9ba2a083547ebbbb81db1afb8eed6beec4f99f08
Jacek <jacek.paszek@onet.pl>
2017-06-10 22:27:35 +0200

    smal gruntfile modifications

--- gruntfile.js
+++ gruntfile.js
@@ -1,26 +1,26 @@
 module.exports = function(grunt) {
 
-// Project configuration.
-grunt.initConfig({
+  // Project configuration.
+  grunt.initConfig({
     jshint: {
-        all: ['js/*.js']
+      all: ['js/*.js']
     },
 
     watch: {
-        scripts: {
-            files: ['js/*.js'],
-            tasks: ['jshint'],
-            options: {
+      scripts: {
+          files: ['js/*.js'],
+          tasks: ['jshint'],
+          options: {
             spawn: false,
-            },
-        },
+          },
+      },
     },
   	  
-});
-// Load the plugins tasks 
-grunt.loadNpmTasks('grunt-contrib-jshint');
-grunt.loadNpmTasks('grunt-contrib-watch');
+  });
+  // Load the plugins tasks 
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   
-// Default task(s).
-grunt.registerTask('default', ['jshint', 'watch']);
+  // Default task(s).
+  grunt.registerTask('default', ['jshint', 'watch']);
 };
